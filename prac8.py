e9286d8bfba4c6c283b42e9645dd32ba26e8bd94
Nisarg Shah <96730708+Nisarg8803@users.noreply.github.com>
2022-01-15 21:23:00 +0530

    Create prac8.py

---
+++ prac8.py
@@ -0,0 +1,7 @@
+print("D21CE178, Nisarg Shah");
+
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+print("Union of A and B:", A.union(B))
+print("Intersection of A and B:", A.intersection(B))
+print("Difference of A and B:", A.difference(B))
