6fc1e75b14318a2f51fc82655bf0420640e940f3
Nisarg Shah <96730708+Nisarg8803@users.noreply.github.com>
2022-03-19 22:57:04 +0530

    Create stackdata.py

---
+++ stackdata.py
@@ -0,0 +1,18 @@
+STACK = []
+
+STACK.append('A')
+STACK.append('B')
+STACK.append('C')
+
+print ('initial stack')
+print(STACK)
+
+print('\nelement popped from stack')
+print(STACK.pop())
+print(STACK.pop())
+print(STACK.pop())
+
+
+print('\nstack after element are popped:')
+print(STACK)
+
